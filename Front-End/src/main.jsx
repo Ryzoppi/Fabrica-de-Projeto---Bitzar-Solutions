d65c0d0d864c916dea5bfc5cef815e10835cb78b
Hugo Facchini <127160695+UInfinitu@users.noreply.github.com>
2026-06-09 23:32:56 -0300

    improvements(react-helmet): Alterando titulo da aba e logo com react-helmet

--- Front-End/src/main.jsx
+++ Front-End/src/main.jsx
@@ -3,14 +3,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { CssBaseline } from '@mui/material'
+import { HelmetProvider } from 'react-helmet-async'
 
 import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CssBaseline />
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <HelmetProvider>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
   </StrictMode>,
 )
